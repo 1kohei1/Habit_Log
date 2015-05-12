d00f2542dace86e049a409ce351e3710513ecb38
Kohei Arai <tobecomebig@gmail.com>
2015-05-12 17:22:22 -0400

    Implemented showing calendar.

--- Habit Log/ViewController.swift
+++ Habit Log/ViewController.swift
@@ -10,16 +10,83 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var monthLabel: UILabel!
+    @IBOutlet weak var menuView: CVCalendarMenuView!
+    @IBOutlet weak var calendarView: CVCalendarView!
+    
+    var animationFinished = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.monthLabel.text = CVDate(date: NSDate()).globalDescription
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        self.menuView.commitMenuViewUpdate()
+        self.calendarView.commitCalendarViewUpdate()
+    }
+}
 
-
+extension ViewController: CVCalendarViewDelegate, CVCalendarMenuViewDelegate {
+    func presentationMode() -> CalendarMode {
+        return .MonthView
+    }
+    
+    func firstWeekday() -> Weekday {
+        return .Sunday
+    }
+    
+    func didSelectDayView(dayView: CVCalendarDayView) {
+        let date = dayView.date
+        println("\(calendarView.presentedDate.commonDescription) is selected!")
+    }
+    
+    func presentedDateUpdated(date: CVDate) {
+        if monthLabel.text != date.globalDescription && self.animationFinished {
+            let updatedMonthLabel = UILabel()
+            updatedMonthLabel.textColor = monthLabel.textColor
+            updatedMonthLabel.font = monthLabel.font
+            updatedMonthLabel.textAlignment = .Center
+            updatedMonthLabel.text = date.globalDescription
+            updatedMonthLabel.sizeToFit()
+            updatedMonthLabel.alpha = 0
+            updatedMonthLabel.center = self.monthLabel.center
+            
+            let offset = CGFloat(48)
+            updatedMonthLabel.transform = CGAffineTransformMakeTranslation(0, offset)
+            updatedMonthLabel.transform = CGAffineTransformMakeScale(1, 0.1)
+            
+            UIView.animateWithDuration(0.35, delay: 0, options: UIViewAnimationOptions.CurveEaseIn, animations: {
+                self.animationFinished = false
+                self.monthLabel.transform = CGAffineTransformMakeTranslation(0, -offset)
+                self.monthLabel.transform = CGAffineTransformMakeScale(1, 0.1)
+                self.monthLabel.alpha = 0
+                
+                updatedMonthLabel.alpha = 1
+                updatedMonthLabel.transform = CGAffineTransformIdentity
+                
+                }) { _ in
+                    
+                    self.animationFinished = true
+                    self.monthLabel.frame = updatedMonthLabel.frame
+                    self.monthLabel.text = updatedMonthLabel.text
+                    self.monthLabel.transform = CGAffineTransformIdentity
+                    self.monthLabel.alpha = 1
+                    updatedMonthLabel.removeFromSuperview()
+            }
+            
+            self.view.insertSubview(updatedMonthLabel, aboveSubview: self.monthLabel)
+        }
+    }
+    
 }
 
